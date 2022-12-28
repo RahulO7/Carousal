ad1c79653eb8be0a3464f89cb74c92a2b43db0c9
Rahul Sharma <rahulpancholi3937@gmail.com>
2022-12-28 10:51:40 +0530

    first commit

---
+++ src/Components/Data/Index.js
@@ -0,0 +1,34 @@
+import A from "../Assests/A.jpg";
+import B from "../Assests/B.jpg";
+import E from "../Assests/E.jpg";
+import C from "../Assests/C.jpg";
+import D from "../Assests/D.jpg";
+import F from "../Assests/F.jpg";
+export const data_images=[
+    {
+        id:1,
+        image:A,
+    },
+    {
+        id:2,
+        image:B,
+    },
+    {
+        id:3,
+        image:C,
+    },
+    {
+        id:4,
+        image:D,
+    },
+    {
+        id:5,
+        image:E,
+    },
+    {
+        id:6,
+        image:F,
+    },
+
+
+]
